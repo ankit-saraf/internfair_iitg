84d78a0213982924b6fbaec2697cacb35675c932
Ankit Saraf <ankitntse@gmail.com>
2020-12-23 15:56:23 +0530

    dcs

--- internfair_iitg/wsgi.py
+++ internfair_iitg/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+# from whitenoise import WhiteNoise
 
 
 
@@ -18,4 +18,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internfair_iitg.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+# application = WhiteNoise(application)
