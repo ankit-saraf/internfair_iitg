472cef645218da714c32c01c52c1544291c33e78
Ankit Saraf <ankitntse@gmail.com>
2020-12-25 16:06:21 +0530

    database setup

--- internfair_iitg/settings.py
+++ internfair_iitg/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+from decouple import config
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -90,12 +92,10 @@ WSGI_APPLICATION = 'internfair_iitg.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
